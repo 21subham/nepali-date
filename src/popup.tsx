bbfd7977cc0f95cbbd9e149e30babb5a43835102
21subham <subhamrajbhandari@gmail.com>
2025-01-27 14:05:33 +0545

    context removed

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react"
+
 import { CountButton } from "~features/count-button"
 
 import "~style.css"
 
 function IndexPopup() {
+  const [day, setDay] = useState("")
+  const getData = async () => {
+    const data = await import("./data/2081/1.json", {
+      assert: { type: "json" }
+    })
+    console.log(data.days.find((day) => day.e === "01"))
+    // Access the JSON content
+    const days = data.days.find((day) => day.e === "01")
+    setDay(days.t)
+  }
+  getData()
   return (
-    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
+    <div className="plasmo-flex plasmo-flex-col plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
       <CountButton />
+      <p>Lauda {day}</p>
     </div>
   )
 }
